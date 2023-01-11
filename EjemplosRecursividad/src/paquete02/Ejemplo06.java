faa54e6c811d899b07b6dd1b5ad4c38fe3ddf5bd
David <adamsmontoya19@gmail.com>
2023-01-11 10:03:20 -0500

    Trabajo en clase 05

--- EjemplosRecursividad/src/paquete02/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo06.java
@@ -10,13 +10,29 @@ public class Ejemplo06 {
 
         int[] valor1 = {100, 1000, 200, 2000, 3000};
         int[] valor2 = {1, 2, 3, 4, 5};
-        
-        // Encontrar la suma de las posiciones
-        // [0][0]
-        // [1][1]
-        // hasta el final
 
-    }
+        //SOLUCION UNO
+        //for (int i = 0; i < valor1.length; i++)
+        //System.out.printf("El valor es: %d\n", obtenerSuma(valor1[i], valor2[i]));
+        
+        //SOLUCION DOS
+        //for (int i = 0; i < valor1.length; i++) {
+            //int num1 = valor1[i];
+            //int num2 = valor2[i];
+            //System.out.printf("El valor es: %d\n", obtenerSuma(num1, num2));
+                
+        //SOLUCION TRES
+
+            for (int i = 0; i < valor1.length; i++) {
+                int num1 = valor1[i];
+                int num2 = valor2[i];
+                int suma = obtenerSuma(num1, num2);
+                System.out.printf("El valor es: %d\n", suma);
+
+            }
+        }
+
+    
 
     public static int obtenerSuma(int a, int b) {
         if (b == 0) {

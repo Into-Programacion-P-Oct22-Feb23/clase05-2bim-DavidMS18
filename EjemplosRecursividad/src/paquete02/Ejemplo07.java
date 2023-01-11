8f5ef51ecfec57de9d7c36f0b5c430ab96af5c02
David <adamsmontoya19@gmail.com>
2023-01-11 10:45:34 -0500

    TrabajoClase05

--- EjemplosRecursividad/src/paquete02/Ejemplo07.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo07.java
@@ -7,7 +7,7 @@ package paquete02;
 public class Ejemplo07 {
 
     public static void main(String[] args) {
-        int[] valores = {2,3,4,5,6,7,8};
+        int[] valores = {2, 3, 4, 5, 6, 7, 8};
         int[] potencias = {2, 3};
         // Por cada posición del arreglo valores
         // obtener su potencia, en función de los valores
@@ -17,15 +17,22 @@ public class Ejemplo07 {
         // se debería presentar el resultado de
         // 2 elevado a la potencia 2 (cuadrado)
         // 2 elevado a la potencia 3 (cubo)
-        
+        for (int i = 0; i < valores.length; i++) {
+            int num = valores[i];
+            int num2 = potencias[0];
+            int num3 = potencias[1];
+            System.out.printf("%d Elevado a la potencia %d = %d\n", valores[i], potencias[0], obtenerPotencia(num, num2));
+            System.out.printf("%d Elevado a la potencia %d = %d\n", valores[i], potencias[1], obtenerPotencia(num, num3));
+        }
+
     }
 
     public static int obtenerPotencia(int base, int exponente) {
-        
+
         if (exponente == 1) {
             return base;
-        } else{
-            
+        } else {
+
             return base * obtenerPotencia(base, exponente - 1);
         }
     }
--- EjemplosRecursividad/src/paquete02/Ejemplo08.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo08.java
@@ -4,9 +4,25 @@
  */
 package paquete02;
 
+import java.util.Scanner;
 public class Ejemplo08 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        int termino =0;
+        
+        System.out.println("Ingrese el tamaño de su arreglo: ");
+            termino = entrada.nextInt();
+            int[] arreglo = new int [termino];
+            
+            for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingresar el valor de la posicion " + i);
+            arreglo[i] = entrada.nextInt();
+        }
+
+        System.out.printf("La suma es igual a: %s\n",
+                misterio(arreglo, termino));
+        
         // Ingresar por teclado el tamaño de un arreglo
         // Ingresar por teclado los valores enteros del arreglo
         // Sumar los valores del arreglo, mediante la función misterio
